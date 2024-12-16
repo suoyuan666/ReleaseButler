8c396df820ee3d5f4387d5e8f60052092b9e7046
suoyuan666 <107405806+suoyuan666@users.noreply.github.com>
2024-12-16 11:03:35 +0800

    [CMake] [utils/misc] [.vscode]: try to import remote repo url for Debian

--- src/include/utils/misc.h
+++ src/include/utils/misc.h
@@ -6,3 +6,7 @@ constexpr std::string_view VERSION = "1.0";
 constexpr std::string_view PACK_NAME = "releasebutler";
 constexpr std::string_view RB_MES_PREV = "releasebutler: ";
 constexpr std::string_view RB_DES = "releasebutler: package manager on GitHub.";
+
+#ifdef DEBIAN
+constexpr std::string_view REMOTE_URL = "https://github.com/suoyuan666/ReleaseButler-pkgs_DEBIAN";
+#endif
